c094a1bfaa858e51342f7d1b64190f9850dc4ee3
Conor Hughes <conreb58@hotmail.co.uk>
2022-05-14 00:22:30 +0100

    Hour display added

--- Clock.js
+++ Clock.js
@@ -1,17 +1,19 @@
+const divContainer = document.getElementById("divContainer");
+
 function makeNumHour(){
     for(var i=0; i<60; i++){
         var newDiv = document.createElement("div");
-        newDiv.id = +i;
-        newDiv.className = "Hour";
+        newDiv.id = "h"+i;
+        newDiv.className = "hourItem";
         newDiv.innerHTML = i
         if (i < 10){
             newDiv.innerHTML = "0"+i
             console.log("add")
-            document.body.appendChild(newDiv)
+            divContainer.appendChild(newDiv)
         }
         else{
             newDiv.innerHTML = i
-            document.body.appendChild(newDiv)
+            divContainer.appendChild(newDiv)
             console.log("add")
         }
     }
@@ -20,20 +22,21 @@ function makeNumHour(){
 function makeNumMin(){
     for(var i=0; i<60; i++){
         var newDiv = document.createElement("div");
-        newDiv.id = +i;
-        newDiv.className = "Min";
+        newDiv.id = "min"
+        newDiv.className = "m"+i;
+        newDiv.classList.add("gridContainer")
         if (i < 10){
             newDiv.innerHTML = "0"+i
             console.log("add")
-            document.body.appendChild(newDiv)
+            divContainer.appendChild(newDiv)
         }
         else{
             newDiv.innerHTML = i
-            document.body.appendChild(newDiv)
+            divContainer.appendChild(newDiv)
             console.log("add")
         }
     }
 }
 
 makeNumHour()
-makeNumMin()
+//makeNumMin()
