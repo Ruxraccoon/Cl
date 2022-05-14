55e5a3f433139b85100b72cefd027807855c23f9
Conor Hughes <conreb58@hotmail.co.uk>
2022-05-14 10:33:38 +0100

    Javascript now displays time correctly, CSS updated,Font added to index

--- Clock.js
+++ Clock.js
@@ -12,11 +12,12 @@ var sixNums = [1,2,3,11,21,31,32,33,41,43,53,52,51];
 var sevenNums = [1,2,3,13,23,33,43,53];
 var eightNums = [1,2,3,11,13,21,23,31,32,33,41,43,51,52,53,22];
 var nineNums = [1,2,3,11,13,21,22,23,33,43,53];
+var numDic = [zeroNums,oneNums,twoNums,threeNums,fourNums,
+fiveNums,sixNums,sevenNums,eightNums,nineNums]
 var getTime = new Date();
 let hour = getTime.getHours();
 let min = getTime.getMinutes();
 let sec = getTime.getSeconds();
-let pos = 0
 
 function makeNumHour(){
     for(var i=0; i<60; i++){
@@ -90,6 +91,7 @@ function updateTime(){
             sec++;
             hour++
         }
+        console.log(min)
     }, 1000);
 }
 
@@ -103,7 +105,7 @@ function changeColor(position){
 function clean(){
     document.getElementById("divContainer")
     .classList.remove("colorChange1");
-    document.getElementById(position)
+    document.getElementById("divContainer")
     .classList.remove("bold");
 }
 
@@ -121,9 +123,41 @@ function draw(position,NumArray,ID){
     }
 }
 
+function displayHour(){
+
+    setInterval(function(){
+        if(hour<10){
+            draw(0,zeroNums,hID)
+            draw(1,numDic[hour],hID)
+        }
+        else if(hour>9){
+            var digits = (""+hour).split("");
+            draw(0,oneNums,hID)
+            draw(1,numDic[digits[1]],hID)
+        }
+    },1000)
+}
+
+function displayMins(){
+
+    setInterval(function(){
+        if(min<10){
+            draw(0,zeroNums,mID)
+            draw(1,numDic[min],mID)
+        }
+        else if(min>9){
+            var digits = (""+min).split("");
+            draw(0,numDic[digits[0]],mID)
+            draw(1,numDic[digits[1]],mID)
+        }
+    },1000)
+}
+
 makeNumHour()
 makeNumMin()
 genHours()
 genMins()
 updateTime()
-draw(0,sevenNums,hID)
+displayHour()
+displayMins()
+
